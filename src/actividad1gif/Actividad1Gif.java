783cac5039d8fefd982c3c207317d4f88e8e6bac
Johnny Reveles <Johnny Reveles@LAPTOP-PO4S021M>
2021-04-20 20:36:52 -0500

    Actividad 1 if - Años

---
+++ src/actividad1gif/Actividad1Gif.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad1gif;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Johnny Reveles
+ */
+public class Actividad1Gif {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    
+    int año1 = 0;
+    int año2 = 2021;
+            
+    Scanner miConsola = new Scanner(System.in);
+    
+    System.out.println("Introduce un año: ");        
+    año1 = miConsola.nextInt();
+    
+    if (año1>año2)
+    
+    
+    
+            
+    
+    
+}
